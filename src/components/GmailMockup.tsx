b982836dc340272194cb2440ecee093518a51c16
Joe Balzac <jgbalzac@gmail.com>
2025-03-03 15:57:24 -0500

    fast building todo list

--- src/components/GmailMockup.tsx
+++ src/components/GmailMockup.tsx
@@ -1,50 +1,52 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 
 interface Email {
-  address: string;
-  from: string;
   id: string;
-  message: string;
-  subject: string;
-  time: string;
   read: string;
+  address: string;
+  message: string;
+  from: string;
+  subject: string;
 }
 
 const GmailMockup = () => {
   const [emails, setEmails] = useState<Email[]>([]);
   const [selectedEmail, setSelectedEmail] = useState<Email | null>(null);
   const [selectedEmailIds, setSelectedEmailIds] = useState<string[]>([]);
-  const [error, setError] = useState("");
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  const fetchEmails = async () => {
+    try {
+      setIsLoading(true);
+      const response = await axios.get(
+        "https://gist.githubusercontent.com/Jsarihan/d5f8cd2d159d676fbfb2fab94750635e/raw/b54cc1bd819b157a93bde00fe059825002f1f602/email.json"
+      );
+      const data: Email[] = response.data;
+      setEmails(data);
+    } catch (error) {
+      setError("An unknown error has occurred");
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   useEffect(() => {
-    const fetchTodos = async () => {
-      try {
-        setIsLoading(true);
-        const response = await axios.get(
-          "https://gist.githubusercontent.com/Jsarihan/d5f8cd2d159d676fbfb2fab94750635e/raw/b54cc1bd819b157a93bde00fe059825002f1f602/email.json"
-        );
-        const data = response.data;
-        setEmails(data);
-        console.log("big data dawg", data);
-      } catch (error) {
-        if (error instanceof Error) {
-          setError("There is an unknown error");
-        }
-      } finally {
-        setIsLoading(false);
-      }
-    };
-
-    fetchTodos();
+    fetchEmails();
   }, []);
 
-  const handleSelectedEmail = (email: Email) => {
+  const handleSelectedEmail = (email: Email, id: string) => {
     setSelectedEmail(email);
     setEmails(
-      emails.map((e) => (e.id === email.id ? { ...email, read: "true" } : e))
+      emails.map((email) =>
+        email.id === id ? { ...email, read: "true" } : email
+      )
     );
+  };
+
+  const handleDeleteEmail = (id: string) => {
+    setEmails(emails.filter((email) => email.id !== id));
   };
 
   const handleInputChange = (
@@ -54,7 +56,7 @@ const GmailMockup = () => {
     setSelectedEmailIds(
       e.target.checked
         ? [...selectedEmailIds, id]
-        : selectedEmailIds.filter((emailId) => emailId !== id)
+        : selectedEmailIds.filter((emailId) => emailId! == id)
     );
   };
 
@@ -65,60 +67,59 @@ const GmailMockup = () => {
         read: selectedEmailIds.includes(email.id) ? "true" : email.read,
       }))
     );
+    setSelectedEmailIds([]);
   };
 
-  const allSelectedAreRead = selectedEmailIds.every(
+  const allEmailsAreRead = selectedEmailIds.every(
     (id) => emails.find((email) => email.id === id)?.read === "true"
   );
 
   return (
     <div>
-      <div>
-        {isLoading ? (
-          <div>Loading.....</div>
-        ) : (
-          <div style={{ display: "flex" }}>
-            <div style={{ flex: 1 }}>
-              <button onClick={toggleReadStatus}>
-                {allSelectedAreRead ? "Mark as unread" : "Mark as read"}
-              </button>
-              <ul>
-                {emails.map((email) => (
-                  <li key={email.id} style={{backgroundColor: email.read === 'true' ? '#FFFFFF' : '#FFC0CB'}}>
-                    <h3>
-                      {email.address} - {email.from} - {email.time}
-                    </h3>
-                    <button
-                      onClick={() => {
-                        handleSelectedEmail(email);
-                      }}
-                    >
-                      Select Email
-                    </button>
-                    <input
-                      type="checkbox"
-                      onChange={(e) => {
-                        handleInputChange(e, email.id);
-                      }}
-                    />
-                  </li>
-                ))}
-              </ul>
-            </div>
-            <div style={{ flex: 2 }}>
-              {selectedEmail ? (
-                <div>
-                  <h3>{selectedEmail.subject}</h3>
-                  <h3>{selectedEmail.from}</h3>
-                  <p>{selectedEmail.message}</p>
-                </div>
-              ) : (
-                <div>Please select an email</div>
-              )}
-            </div>
+      {isLoading ? (
+        <div>Loading....</div>
+      ) : (
+        <div className="grid grid-cols-2">
+          <div>
+            <button onClick={toggleReadStatus}>
+              {allEmailsAreRead ? "Mark as unread" : "Mark as read"}
+            </button>
+            <ul>
+              {emails.map((email) => (
+                <li
+                  key={email.id}
+                  style={{
+                    backgroundColor: email.read === "true" ? "#fff" : "#fafafb",
+                  }}
+                >
+                  <h2>{email.from}</h2>
+                  <h3>{email.subject}</h3>
+
+                  <button onClick={() => handleSelectedEmail}>Select</button>
+                  <button onClick={() => handleDeleteEmail(email.id)}>
+                    Delete
+                  </button>
+                  <input
+                    type="checkbox"
+                    onChange={(e) => handleInputChange(e, email.id)}
+                  />
+                </li>
+              ))}
+            </ul>
           </div>
-        )}
-      </div>
+
+          {selectedEmail ? (
+            <div>
+              <h2>{selectedEmail.subject}</h2>
+              <h3>{selectedEmail.from}</h3>
+              <p>{selectedEmail.message}</p>
+            </div>
+          ) : (
+            <div>Please select an email</div>
+          )}
+        </div>
+      )}
+      {error && <p>{error}</p>}
     </div>
   );
 };
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,8 +1,10 @@
+import axios from "axios";
 import { useEffect, useState } from "react";
 
 interface Todo {
   id: number;
   title: string;
+  completed: boolean;
 }
 
 const TodoList = () => {
@@ -10,28 +12,25 @@ const TodoList = () => {
   const [newTodo, setNewTodo] = useState("");
   const [editingId, setEditingId] = useState<number | null>(null);
   const [editingText, setEditingText] = useState("");
-  const [error, setError] = useState("");
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  const fetchTodos = async () => {
+    try {
+      setIsLoading(true);
+      const response = await axios.get(
+        "https://jsonplaceholder.typicode.com/todos?_limit=10"
+      );
+      const data: Todo[] = response.data;
+      setTodos(data);
+    } catch (error) {
+      setError("An unknown error has occurred");
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   useEffect(() => {
-    const fetchTodos = async () => {
-      try {
-        setIsLoading(true);
-        const response = await fetch(
-          "https://jsonplaceholder.typicode.com/todos?_limit=10"
-        );
-        const data: Todo[] = await response.json();
-        setTodos(data);
-        console.log("Big Data", data);
-      } catch (error) {
-        if (error instanceof Error) {
-          setError("An unknow error has occured");
-        }
-      } finally {
-        setIsLoading(false);
-      }
-    };
-
     fetchTodos();
   }, []);
 
@@ -40,12 +39,10 @@ const TodoList = () => {
       const newTodoItem: Todo = {
         id: Date.now(),
         title: newTodo.trim(),
+        completed: false,
       };
       setTodos([...todos, newTodoItem]);
       setNewTodo("");
-    } else {
-      setError("please add a todo");
-      setTimeout(() => setError(""), 3000);
     }
   };
 
@@ -59,59 +56,45 @@ const TodoList = () => {
   };
 
   const handleSaveTodo = () => {
-    if (editingId !== null && editingText.trim()) {
-      setTodos(
-        todos.map((todo) =>
-          todo.id === editingId ? { ...todo, title: editingText } : todo
-        )
-      );
-      setEditingId(null);
-    }
+    setTodos(
+      todos.map((todo) =>
+        editingId === todo.id ? { ...todo, title: editingText } : todo
+      )
+    );
+    setEditingId(null);
   };
 
   return (
     <div>
-      <input
-        value={newTodo}
-        type="text"
-        onChange={(e) => {
-          setNewTodo(e.target.value);
-        }}
-      />
-      <button onClick={handleAddTodo}>Add Todo +</button>
-      {error && <p style={{ color: "red" }}>{error}</p>}
       {isLoading ? (
-        <div>isLoading....</div>
+        <div>Loading....</div>
       ) : (
         <div>
+          <input
+            type="text"
+            value={newTodo}
+            onChange={(e) => setNewTodo(e.target.value)}
+          />
+          <button onClick={handleAddTodo}>Add Todo +</button>
           <ul>
             {todos.map((todo) => (
               <li key={todo.id}>
                 {editingId === todo.id ? (
                   <div>
                     <input
-                      value={editingText}
                       type="text"
+                      value={editingText}
                       onChange={(e) => setEditingText(e.target.value)}
                     />
                     <button onClick={handleSaveTodo}>Save</button>
                   </div>
                 ) : (
                   <div>
-                    {" "}
                     {todo.title}
-                    <button
-                      onClick={() => {
-                        handleDeleteTodo(todo.id);
-                      }}
-                    >
+                    <button onClick={() => handleDeleteTodo(todo.id)}>
                       Delete
                     </button>
-                    <button
-                      onClick={() => {
-                        handleEditTodo(todo.id, todo.title);
-                      }}
-                    >
+                    <button onClick={() => handleEditTodo(todo.id, todo.title)}>
                       Edit
                     </button>
                   </div>
@@ -119,6 +102,7 @@ const TodoList = () => {
               </li>
             ))}
           </ul>
+          {error && <p style={{ color: "red" }}>{error}</p>}
         </div>
       )}
     </div>
