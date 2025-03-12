924dfbbe83dd319b4f7aa9c259d168cf3748c23e
Joe Balzac <jgbalzac@gmail.com>
2025-03-12 14:38:02 -0400

    keep pushing forward

--- src/components/SocialMediaPost.tsx
+++ src/components/SocialMediaPost.tsx
@@ -1,9 +1,29 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
+
+interface User {
+  userId: number;
+  name: string;
+}
+
+interface Post {
+  id: number;
+  title: string;
+  body: string;
+}
+
+interface Comment {
+  postId: number;
+  id: number;
+  name: string;
+  email: string;
+  body: string;
+}
 
 const SocialMediaPost = () => {
-  const [posts, setPosts] = useState([]);
-  const [users, setUsers] = useState([]);
-  const [comments, setComments] = useState([]);
+  const [posts, setPosts] = useState<Post[]>([]);
+  const [users, setUsers] = useState<User[]>([]);
+  const [comments, setComments] = useState<Comment[]>([]);
+  const [selectedPostId, setSelectedPostId] = useState<number | null>();
 
   useEffect(() => {
     Promise.all([
@@ -23,9 +43,44 @@ const SocialMediaPost = () => {
     });
   }, []);
 
-  console.log(posts, users, comments);
+  console.log("Posts", posts);
+  console.log("Users", users);
+  console.log("Comments", comments);
 
-  return <div>SocialMediaPost</div>;
+  const handleSelectedPost = (id: number) => {
+    setSelectedPostId(id);
+  };
+
+  return (
+    <div>
+      <ul>
+        {posts.map((post) => {
+          const author = users.find((name) => name.userId === post.id);
+          const postComments = comments.filter(
+            (comment) => comment.id === post.id
+          );
+          return (
+            <li onClick={() => handleSelectedPost(post.id)}>
+              <h2>{post.title}</h2>
+              {author?.name}
+              <p>{post.body}</p>
+
+              {selectedPostId === post.id && (
+                <ul>
+                  {postComments.map((comment) => (
+                    <li>
+                      <h3> {comment.name}</h3>
+                      <p>{comment.body}</p>
+                    </li>
+                  ))}
+                </ul>
+              )}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 };
 
 export default SocialMediaPost;
