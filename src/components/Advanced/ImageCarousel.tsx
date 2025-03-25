d4b16d1d61c0cfd7a183848e4b56c15062dd212b
Joe Balzac <jgbalzac@gmail.com>
2025-03-24 21:54:48 -0400

    adds simpler math

--- src/components/Advanced/ImageCarousel.tsx
+++ src/components/Advanced/ImageCarousel.tsx
@@ -12,19 +12,33 @@ interface Image {
 const ImageCarousel = () => {
   const [images, setImages] = useState<Image[]>([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [index, setIndex] = useState(0);
 
   useEffect(() => {
+    setIsLoading(true);
     fetch("https://picsum.photos/v2/list?page=1&limit=10")
-      .then((res) => {
-        setIsLoading(true);
-        return res.json();
-      })
+      .then((res) => res.json())
       .then((data) => {
         setImages(data);
         setIsLoading(false);
       });
   }, []);
-  console.log("Images bruh", images);
+
+  const handleNextImage = () => {
+    if (index === images.length - 1) {
+      setIndex(0); 
+    } else {
+      setIndex(index + 1);
+    }
+  };
+
+  const handlePrevImage = () => {
+    if (index === 0) {
+      setIndex(images.length - 1);
+    } else {
+      setIndex(index - 1);
+    }
+  };
 
   return (
     <div>
@@ -32,14 +46,18 @@ const ImageCarousel = () => {
         <div>Loading....</div>
       ) : (
         <div>
-          {images.map((image) => (
+          <div>
+            <button onClick={handlePrevImage}>Prev</button>
+            <button onClick={handleNextImage}>Next</button>
+          </div>
+          {images[index] && (
             <img
-              src={(image.download_url)}
-              height={image.height}
-              width={image.width}
-              alt={image.author}
+              src={images[index].download_url}
+              alt={images[index].author}
+              width="600"
+              height="400"
             />
-          ))}
+          )}
         </div>
       )}
     </div>
