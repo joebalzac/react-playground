34c071f06ecea423e9af8c25bfa7f088965351f9
Joe Balzac <jgbalzac@gmail.com>
2025-03-26 15:53:14 -0400

    this is it for all

--- src/components/SocialMediaPost.tsx
+++ src/components/SocialMediaPost.tsx
@@ -1,34 +1,74 @@
+import axios from "axios";
 import { useEffect, useState } from "react";
-const [posts, setPosts] = useState([]);
-const [users, setUsers] = useState([]);
-const [comments, setComments] = useState<Comment[]>([]);
-const [selectedPostId, setSelectedPostId] = useState<number | null>(null);
+
+interface Post {
+  userId: number;
+  id: number;
+  title: string;
+}
+
+interface User {
+  id: number;
+  name: string;
+  username: string;
+}
+
+interface Comment {
+  postId: number;
+  id: number;
+  name: string;
+}
 
 const SocialMediaPost = () => {
+  const [posts, setPosts] = useState<Post[]>([]);
+  const [users, setUsers] = useState<User[]>([]);
+  const [comments, setComments] = useState<Comment[]>([]);
+  const [selectedPost, setSelectedPost] = useState<number | null>(null);
+
   useEffect(() => {
     Promise.all([
-      fetch("https://jsonplaceholder.typicode.com/posts").then((res) =>
-        res.json()
-      ),
-      fetch("https://jsonplaceholder.typicode.com/users").then((res) =>
-        res.json()
-      ),
-      fetch("https://jsonplaceholder.typicode.com/comments").then((res) =>
-        res.json()
-      ),
+      axios
+        .get("https://jsonplaceholder.typicode.com/posts")
+        .then((res) => res)
+        .then((data) => data),
+      axios
+        .get("https://jsonplaceholder.typicode.com/users")
+        .then((res) => res)
+        .then((data) => data),
+      axios
+        .get("https://jsonplaceholder.typicode.com/comments")
+        .then((res) => res)
+        .then((data) => data),
     ]).then(([postsData, usersData, commentsData]) => {
-      setPosts(postsData);
-      setUsers(usersData);
-      setComments(commentsData);
+      setPosts(postsData.data);
+      setUsers(usersData.data);
+      setComments(commentsData.data);
     });
   }, []);
 
-  console.log(users, posts, comments);
+  console.log("here is the data", posts, users, comments);
+
+  const handleSelectedPost = (id: number) => {
+    setSelectedPost(id);
+  };
 
   return (
     <div>
       {posts.map((post) => {
-        return <div>{post}</div>;
+        const author = users.find((user) => user.id === post.id);
+        const postComment = comments.find((comment) => comment.id === post.id);
+        return (
+          <div key={post.id}>
+            {post.title}
+            <button onClick={() => handleSelectedPost(post.id)}>
+              Select Post
+            </button>
+            {selectedPost === post.id && <div>{postComment?.name}</div>}
+            <p>
+              {author?.name} - {author?.username}
+            </p>
+          </div>
+        );
       })}
     </div>
   );
